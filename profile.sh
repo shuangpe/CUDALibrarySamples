6189dd0dfc3f243493d73a8ad151deb38aba9573
Zhou,Shuangpeng <shuangpeng.zhou@intel.com>
2025-08-01 10:22:33 +0000

    Monitor GPU power

--- profile.sh
+++ profile.sh
@@ -7,7 +7,7 @@ SCRIPT_DIR=$(dirname "$(realpath $0)")
 LOG_FILE="$SCRIPT_DIR/profile_log.txt"
 
 # Define problem shapes and transpose configurations
-PROBLEM_SHAPES=("16384x16384x16384" "1024x16384x16384")
+PROBLEM_SHAPES=("16384x16384x16384" "8192x16384x16384" "1024x16384x16384")
 TRANSPOSE_VALUES=("0" "1")
 
 # Define executables to profile
@@ -54,6 +54,28 @@ execute() {
   fi
 }
 
+wl_id=0
+gpu_monitor_cmd="nvidia-smi --id=${GPU_ID} --loop-ms=100 -q -a| grep -v -e{Fan,N/A,JPEG,OFA} > nvsmi.csv"
+
+# NVIDIA SMI logging
+nvsmi_log() {
+  if [ "$DRY_RUN" -eq 0 ]; then
+    if [ "$1" = "start" ]; then
+      echo "Starting GPU monitoring: $gpu_monitor_cmd"
+      eval "$gpu_monitor_cmd" &
+    elif [ "$1" = "stop" ]; then
+      echo "Stopping GPU monitoring"
+      pkill -f nvidia-smi
+      sleep 5
+    fi
+  fi
+}
+
+# Rename log file
+rename_log() {
+  mv "$1" "$2"
+}
+
 # Profile each combination of problem shape, transpose values, random max, and frequency
 for EXECUTABLE in "${EXECUTABLES[@]}"; do
   for PROBLEM_SHAPE in "${PROBLEM_SHAPES[@]}"; do
@@ -67,8 +89,14 @@ for EXECUTABLE in "${EXECUTABLES[@]}"; do
               execute "nvidia-smi --id=\"$GPU_ID\" --lock-gpu-clocks=\"$FREQ\",\"$FREQ\""
             fi
 
+            nvsmi_log start
             cmd="PROBLEM_SHAPE=$PROBLEM_SHAPE TRANS_A=$TRANS_A TRANS_B=$TRANS_B RANDOM_MAX=$RANDOM_MAX ENABLE_PROFILE=1 \"$EXECUTABLE\""
             execute "$cmd"
+            nvsmi_log stop
+
+            rename_log nvsmi.csv "workload${wl_id}_nvsmi.txt"
+            echo "Renamed nvsmi.csv to workload${wl_id}_nvsmi.txt" | tee -a "$LOG_FILE"
+            wl_id=$((wl_id + 1))
 
             if [[ "$FREQ" != "oob" ]]; then
               execute "nvidia-smi --reset-gpu-clocks --id=\"$GPU_ID\""
@@ -91,8 +119,14 @@ if [[ -f "$NVFP4_EXECUTABLE" ]]; then
         execute "nvidia-smi --id=\"$GPU_ID\" --lock-gpu-clocks=\"$FREQ\",\"$FREQ\""
       fi
 
-      cmd="PROBLEM_SHAPE=$PROBLEM_SHAPE TRANS_A=1 TRANS_B=0 RANDOM_MAX=6 ENABLE_PROFILE=1 \"$NVFP4_EXECUTABLE\""
+      nvsmi_log start
+      cmd="PROBLEM_SHAPE=$PROBLEM_SHAPE FREQ=$FREQ TRANS_A=1 TRANS_B=0 RANDOM_MAX=6 ENABLE_PROFILE=1 PROFILE_ITER="1000,20000" \"$NVFP4_EXECUTABLE\""
       execute "$cmd"
+      nvsmi_log stop
+
+      rename_log nvsmi.csv "workload${wl_id}_nvsmi.txt"
+      echo "Renamed nvsmi.csv to workload${wl_id}_nvsmi.txt" | tee -a "$LOG_FILE"
+      wl_id=$((wl_id + 1))
 
       if [[ "$FREQ" != "oob" ]]; then
         execute "nvidia-smi --reset-gpu-clocks --id=\"$GPU_ID\""
